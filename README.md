977b074763889f7e37d1355c581085bbe82644ba
Hayato <hayato@inox.co.th>
2021-11-25 15:34:50 +0700

    add more readme

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # Example of ByteArk Player React without controls
 
-This project is an example usage for disable all controls for ByteArk Player React
+This project is an example usage for disable controls for ByteArk Player React
 
 ## Requirements
-* ByteArk Player React v1.0.0-beta17^
+
+- ByteArk Player React v1.0.0-beta17^
 
 ## Custom
-- Hide all controls button in control bar
-- Hide header bar when title are not included in the source.
+
+- Hide all controls button in control bar (except play/pause and volume button)
+- Hide header bar when title are not included in the source
 - Disable double click to open the full screen
 - Add ended event function
 
 ## Read more
+
 - [ByteArk Player React](https://github.com/byteark/byteark-player-react)
--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ function App() {
       "https://qoder.byteark.com/images/video-frames/1/GU/cg/1GUcgd3XwsmD-large.jpg",
     src: "https://inox-bhm9yr.cdn.byteark.com/video-objects/RI2PimuHxDXw/playlist.m3u8",
     type: "application/x-mpegURL",
-      title: "Big Buck Bunny",
+    title: "Big Buck Bunny",
   };
 
   return (
     <div className="App">
       <span className="title">Custom Player</span>
+      <ul>
+        <li>
+          Hide all controls button in control bar (except play/pause and volume
+          button)
+        </li>
+        <li>Hide header bar when title are not included in the source</li>
+        <li>Disable double click to open the full screen</li>
+        <li>Add ended event function</li>
+      </ul>
       <CustomPlayer source={source} />
     </div>
   );
